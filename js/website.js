b4b0c19f35af9ea51c078bca6eaf2a329d08118a
leban <leban@DESKTOP-NP56T40>
2018-01-18 22:48:28 -0500

    angular framework now works one hunnid

--- js/website.js
+++ js/website.js
@@ -5,16 +5,25 @@ var app = angular.module("website", ['ngRoute']);
 
 app.config(function($routeProvider) {
     $routeProvider
-        .when("/", {
+        .when("/home", {
             templateUrl : "partials/home.html"
         })
         .when("/contact", {
             templateUrl : "partials/contact.html"
-        })
+        }).when("/OurMission", {
+        templateUrl : "partials/OurMission.html"
+    }).when("/", {
+        templateUrl : "partials/home.html"
+    }).when("/service", {
+        templateUrl : "partials/service.html"
+    })
 
     ;
 });
 
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
 function MainCtrl($scope){
 
 }
