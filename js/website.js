c19ccd0cd3f91a403a5a96d3c6b565867527a94e
leban <leban@DESKTOP-NP56T40>
2018-01-18 22:20:09 -0500

    AngularJS amelioration

--- js/website.js
+++ js/website.js
@@ -1,2 +1,20 @@
-var app = angular.module("website", []);
+import ('js/angular.js');
+import ('https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js');
 
+var app = angular.module("website", ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "partials/home.html"
+        })
+        .when("/contact", {
+            templateUrl : "partials/contact.html"
+        })
+
+    ;
+});
+
+function MainCtrl($scope){
+
+}
