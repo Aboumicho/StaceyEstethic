0710b7bc0da6e92bc1b4f358b584897c4f093a5e
leban <leban@DESKTOP-NP56T40>
2018-01-30 22:46:15 -0500

    added few jquery functions and header

--- js/router.js
+++ js/router.js
@@ -5,3 +5,45 @@ function LoadContact(){
 function LoadOurMission(){
     $("#main").load("./partials/OurMission.html");
 }
+function LoadService(){
+    $("#main").load("./partials/service.html");
+}
+
+function LoadFacial(){
+    $("#main").load("./partials/Facial.html");
+}
+function LoadBody(){
+    $("#main").load("./partials/Facial/body.html");
+}
+function LoadCasmara(){
+    $("#main").load("./partials/casmara/casmara.html");
+}
+
+function LoadFacial(){
+    $("#main").load("./partials/Facial.html");
+}
+function LoadBody(){
+    $("#main").load("./partials/OurMission.html");
+}
+
+
+function LoadCinnamon(){
+    $("#main").load("./partials/Facial/Casmara/Cinnamon.html");
+}
+
+function LoadGoji(){
+    $("#main").load("./partials/Facial/Casmara/Goji.html");
+}
+
+function LoadGold(){
+    $("#main").load("./partials/Gold.html");
+}
+
+function LoadRetense(){
+    $("#main").load("./partials/Facial/Casmara/Retense.html");
+}
+
+
+function LoadSensitive(){
+    $("#main").load("./partials/Facial/Casmara/Sensitive.html");
+}
